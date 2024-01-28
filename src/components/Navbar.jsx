1d93e5dc2471ea8283b7ced09dd5499d1d6b9d41
kausarthind <Kausarthind555@gmail.com>
2024-01-29 01:34:00 +0300

    link to home page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
         <div className="container mx-auto flex justify-between items-center">
           <Logo/>
           <div className="hidden md:flex space-x-4 text-gray">
+            <Link to="/" className="nav-item">Home</Link>
             <Link to="/about" className="nav-item">About</Link>
             <Link to="/projects" className="nav-item">Projects</Link>
             <Link to="/services" className="nav-item">Services</Link>
