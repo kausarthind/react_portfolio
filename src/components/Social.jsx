9f4b64996a6348a8105d6bc26f5f12710373b91a
kausarthind <Kausarthind555@gmail.com>
2024-01-29 03:56:06 +0300

    instagram link added

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -6,7 +6,7 @@
  */
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedin, faFacebook, faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter, faLinkedin, faDiscord, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const SocialIcons = () => {
   return (
@@ -14,7 +14,9 @@ const SocialIcons = () => {
     <div className="p-1 rounded-md ml-3">
       <FontAwesomeIcon icon={faTwitter} className="text-gray text-2xl m-2 transition-transform transform hover:scale-110 hover:cursor-pointer hover:text-blue" />
       <FontAwesomeIcon icon={faLinkedin} className="text-gray text-2xl m-2 transition-transform transform hover:scale-110 hover:cursor-pointer hover:text-blue" />
-      <FontAwesomeIcon icon={faFacebook} className="text-gray text-2xl m-2 transition-transform transform hover:scale-110 hover:cursor-pointer hover:text-blue" />
+      <a href='https://www.instagram.com/kausarthind_?utm_source=qr&igsh=MWF1aXNwN3dxYmpubg=='>
+        <FontAwesomeIcon icon={faInstagram} className="text-gray text-2xl m-2 transition-transform transform hover:scale-110 hover:cursor-pointer hover:text-blue" />
+        </a>
       <FontAwesomeIcon icon={faDiscord} className="text-gray text-2xl m-2 transition-transform transform hover:scale-110 hover:cursor-pointer hover:text-blue" />
     </div>
   );
